49aa94cc88bef6739a3a1f596ff7b68bcbe5229b
SEMMOZHI A <asemmozhi.cse2025@citchennai.net>
2025-12-22 16:11:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4288209882-17a1a",
+  "appId": "1:141663560212:web:574cf2c469ea847712bd01",
+  "apiKey": "AIzaSyBzy1mfQFsOXpEFxigFa3e1A2x1XmQp-R8",
+  "authDomain": "studio-4288209882-17a1a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "141663560212"
+};
