54e5e015bfa61b9e2f378d6064636926c894a68f
SEMMOZHI A <asemmozhi.cse2025@citchennai.net>
2026-01-08 18:23:29 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 
 export const firebaseConfig = {
   "projectId": "lunar-tube-482013-i1",
-  "appId": "1:141663560212:web:574cf2c469ea847712bd01",
-  "apiKey": "AIzaSyBYsbp68v9X4JLTF3iMediwNZGl2VG7Eaoi",
+  "appId": "1:291639778966:web:f3eac5a2f7660a5f0dacb7",
+  "apiKey": "AIzaSyBYsbp68v9X4JLTF3iMediwNZGl2VG7Eao",
   "authDomain": "lunar-tube-482013-i1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "141663560212"
+  "measurementId": "G-2H163TQ9VM",
+  "messagingSenderId": "291639778966"
 };
-
