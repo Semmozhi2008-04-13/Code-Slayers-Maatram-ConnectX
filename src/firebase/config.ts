c163a2826f2ae0e6f0a0801add7f328f6faf751c
SEMMOZHI A <asemmozhi.cse2025@citchennai.net>
2026-01-08 17:28:47 +0000

    apap AIzaSyBYsbp68v9X4JLTF3iMediwNZGl2VG7Eaoi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "studio-4288209882-17a1a",
   "appId": "1:141663560212:web:574cf2c469ea847712bd01",
-  "apiKey": "AIzaSyCYavImmxHTHUR6pH179geXV9PFNbApfyc",
+  "apiKey": "AIzaSyBYsbp68v9X4JLTF3iMediwNZGl2VG7Eaoi",
   "authDomain": "studio-4288209882-17a1a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "141663560212"
 };
+
